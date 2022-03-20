a658103327283b29f5f86cfc92f16cb19daed355
Kevin Sparling <kevinsparling@gmail.com>
2022-03-20 23:05:15 +0800

    switched to gatsby, I can import CSV files here

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,45 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-    title: ``,
+    title: `Wild Bill James`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-mdx", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-csv",
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        resetCSS: true,
+        isUsingColorMode: true,
+      }
+    },
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    }, 
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }, 
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: "./src/data"
+      }
+    }
+  ]
 };
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+import Layout from '../layouts/Layout'
+
+const IndexPage = () => {
+  return (
+    <>
+      <Layout>
+        <Box p={8}>
+          <Text fontSize='xl'>Hello</Text>
+        </Box>
+      </Layout>
+      
+    </>
+  )
+}
+
+export default IndexPage
