2b483e0bdad09506e9041c99c4617ff30b536468
Kevin Sparling <kevinsparling@gmail.com>
2022-03-21 09:53:36 +0800

    added managers and react-vis

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Box, Text } from '@chakra-ui/react'
+//import { LineChart, Line, ResponsiveContainer } from 'recharts'
+import {XYPlot, LineSeries, XAxis, YAxis, HorizontalGridLines} from 'react-vis';
+
 import Layout from '../layouts/Layout'
 
 const IndexPage = () => {
@@ -7,12 +10,25 @@ const IndexPage = () => {
     <>
       <Layout>
         <Box p={8}>
-          <Text fontSize='xl'>Hello</Text>
+          <Text fontSize='xl'>{3 + 2}</Text>
         </Box>
+        <XYPlot
+          width={300}
+          height={300}>
+          <HorizontalGridLines />
+          <LineSeries
+            color="red"
+            data={[
+              {x: 1, y: 10},
+              {x: 2, y: 5},
+              {x: 3, y: 15}
+            ]}/>
+          <XAxis title="X" />
+          <YAxis />
+        </XYPlot>
       </Layout>
       
     </>
   )
 }
-
-export default IndexPage
+ export default IndexPage
